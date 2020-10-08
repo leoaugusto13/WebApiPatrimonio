66ca46ef28a6d172e4c21de33caf8c6218aa1145
Leonardo Augusto <50834336+leoaugusto13@users.noreply.github.com>
2020-10-07 23:07:38 -0300

    Create insert.sql

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,5 @@
+insert into tb_usuario(nome,email,senha) values('admin','admin@admin.com','admin');
+
+insert into tb_marca(nome) values('NAVITA');
+
+insert into tb_patrimonio(nome,descricao,marca_id) values('macbook','t.i',1);
